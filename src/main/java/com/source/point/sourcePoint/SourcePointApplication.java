21e54ffdbfa2a29f943f8668f1b2d706935edf50
xitiaoyu <xitiaoyu@evergrande.com>
2019-12-13 15:36:43 +0800

    初始化

---
+++ src/main/java/com/source/point/sourcePoint/SourcePointApplication.java
@@ -0,0 +1,13 @@
+package com.source.point.sourcePoint;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SourcePointApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SourcePointApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/source/point/sourcePoint/SourcePointApplicationTests.java
@@ -0,0 +1,13 @@
+package com.source.point.sourcePoint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SourcePointApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
