700bd044247ba007733f414123e157acef95bd31
xitiaoyu <xitiaoyu@evergrande.com>
2019-12-16 17:51:27 +0800

    将项目改成euraka服务端

--- src/main/java/com/source/point/sourcePoint/SourcePointApplication.java
+++ src/main/java/com/source/point/sourcePoint/SourcePointApplication.java
@@ -2,8 +2,10 @@ package com.source.point.sourcePoint;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class SourcePointApplication {
 
 	public static void main(String[] args) {
