f8e866585e72cac6b5aa2f429475df809dc1cefa
xitiaoyu <xitiaoyu@evergrande.com>
2019-12-25 10:54:01 +0800

    加入log4j2日志,暂时去掉eureka服务

--- src/main/java/com/source/point/sourcePoint/SourcePointApplication.java
+++ src/main/java/com/source/point/sourcePoint/SourcePointApplication.java
@@ -2,10 +2,9 @@ package com.source.point.sourcePoint;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+//@EnableEurekaServer 暂时注释掉Eureka服务
 public class SourcePointApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/source/point/sourcePoint/web/BasicDataController.java
+++ src/main/java/com/source/point/sourcePoint/web/BasicDataController.java
@@ -1,5 +1,7 @@
 package com.source.point.sourcePoint.web;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,10 +12,12 @@ import io.swagger.annotations.Api;
 @RequestMapping("/api")
 @Api(description = "基础数据相关", tags = "BASIC-DATA")
 public class BasicDataController {
-
+	private static final Logger log = LoggerFactory.getLogger(BasicDataController.class);
 	@GetMapping("/query")
 	public String query() {
-		return "好的--->"+System.currentTimeMillis();
+		String str = "好的--->"+System.currentTimeMillis();
+		log.info(str);
+		return str;
 	}
 
 }
